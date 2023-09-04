1370b711357c2164c9ccd9f4113960255f003e12
Rabouz <rabbouzzakr@gmail.com>
2023-09-04 11:44:29 -0700

    concat edges

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+first_sentence = str[39:66]
+second_sentence = str[107:111]
+print(f"{first_sentence} {second_sentence} Python")
