315a5b5f85049e0019df9836d82b7c6145cf328a
Rabouz <rabbouzzakr@gmail.com>
2023-09-04 11:52:37 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-first_sentence = str[39:66]
-second_sentence = str[107:111]
-print(f"{first_sentence} {second_sentence} Python")
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + " " + str[107:111] + " " + str[0:6])
