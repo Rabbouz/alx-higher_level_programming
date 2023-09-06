5e33e62b7f029e872d93a7a68fff1f58eb70bbe8
Rabouz <rabbouzzakr@gmail.com>
2023-09-05 17:32:58 -0700

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - (c % 32))), end="")
+    if c % 2 == 0:
+        print("{}".format(chr(c).lower()), end="")
+    else:
+        print("{}".format(chr(c).upper()), end="")
