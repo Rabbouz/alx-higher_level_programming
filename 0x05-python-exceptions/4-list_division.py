0f328b5517ad4b34fffcfd7d22fa74def84488e3
Rabbouz <rabbouzzakr@gmail.com>
2023-09-25 23:56:51 +0100

    division of listss and max lenght of list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for itr in range(0, list_length):
+        try:
+            division = my_list_1[itr] / my_list_2[itr]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            division = 0
+        except IndexError:
+            print("out of range")
+            division = 0
+        finally:
+            new_list.append(division)
+    return (new_list)
