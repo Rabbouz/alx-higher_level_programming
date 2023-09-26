5f181e676f85236030cb18d6f3b9e757c98076b8
Rabbouz <rabbouzzakr@gmail.com>
2023-09-26 23:39:44 +0100

    update of pycodestyle

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,9 @@
 
 class Square:
     """Representing a square"""
+
     def __init__(self, size=0):
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
