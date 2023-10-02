4a9cb70f79ca168d49531caf80fa7c5360e91a58
Rabbouz <rabbouzzakr@gmail.com>
2023-10-02 22:02:34 +0100

    README & rectangle task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# festus sulumeti
+"""
+    Simple rectangle : an empty class Rectangle that defines a rectangle
+    It is not allowed to import any module
+"""
+class Rectangle(object):
+    """Rectangle: Define new class"""
+    pass
