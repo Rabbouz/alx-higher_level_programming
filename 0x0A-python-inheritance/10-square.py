1cc35c869f7896d0ede0c6f3198e8680e9b2b843
Rabouz <rabbouzzakr@gmail.com>
2023-10-10 00:55:05 +0100

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining a class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+def __init__(self, size):
+    self.size = ("size", size)
+    self.integer_validator(size)
+    
+def area(self):
+    return self.__size * self.__size
+
+def __str__(self):
+    string = "[" + str(self.na__name__.__name__) + "]"
+    string += str(self.__size) + "/" + str(self.__size)
+    return string
