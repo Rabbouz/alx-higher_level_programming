5ad070e36da60ae7d9de7c23327ff517cd881045
Rabouz <rabbouzzakr@gmail.com>
2023-10-10 17:11:02 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Reading the file text 'UTF8' and printing it to stdout"""
+"""Defning a text file(reading) function"""
+
 
 def read_file(filename=""):
+    """Reading the file text 'UTF8' and printing it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
